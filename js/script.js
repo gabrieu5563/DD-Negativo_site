b471feb4cc73a593311d9529a1d10eb777398134
Gabriel <gabriel.joaquim.5567@gmail.com>
2023-11-15 21:51:32 -0300

    feat: adicionei a página de contato e finalizei a página inicial

---
+++ js/script.js
@@ -0,0 +1,27 @@
+function copiar_tel1(){
+    let tel = document.getElementById("tel1").value
+    navigator.clipboard.writeText(tel)
+    alert("Número copiado")
+    console.log(tel)
+}
+
+function copiar_tel2(){
+    let tel = document.getElementById("tel2").value
+    navigator.clipboard.writeText(tel)
+    alert("Número copiado")
+    console.log(tel)
+}
+
+function copiar_wpp1(){
+    let tel = document.getElementById("wpp1").value
+    navigator.clipboard.writeText(tel)
+    alert("Número copiado")
+    console.log(tel)
+}
+
+function copiar_wpp2(){
+    let tel = document.getElementById("wpp2").value
+    navigator.clipboard.writeText(tel)
+    alert("Número copiado")
+    console.log(tel)
+}
