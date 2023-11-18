2b750b347ce9fcb7c5778878c2ab7ab294dcde9b
Gabriel <gabriel.joaquim.5567@gmail.com>
2023-11-18 15:01:20 -0300

    feat: adicionada pagina de orçamento
    adicionada primeira parte da página de orçamento e correção do botão 'saiba mais' da página inicial

--- js/script.js
+++ js/script.js
@@ -25,3 +25,26 @@ function copiar_wpp2(){
     alert("Número copiado")
     console.log(tel)
 }
+
+function copiar_mail1(){
+    let tel = document.getElementById("mail1").value
+    navigator.clipboard.writeText(tel)
+    alert("E-mail copiado")
+    console.log(tel)
+}
+
+function redirect() {
+    window.location.href = "orcamento.html";
+}
+
+let botao_clicado = 0;
+
+function selection(buttonId, valor) {
+    document.querySelectorAll('.option2').forEach(button => {
+    button.classList.remove('clicked');
+    });
+    document.getElementById(buttonId).classList.add('clicked');
+
+    botao_clicado = valor;
+    console.log(botao_clicado);
+}
