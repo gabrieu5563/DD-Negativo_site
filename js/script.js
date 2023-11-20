d90703f155403c97e39153337d4883e27830f2c6
Gabriel <gabriel.joaquim.5567@gmail.com>
2023-11-20 16:14:17 -0300

    feat: adicionada a página de orçamento de computador
    adicionada a página de orçamento de computador, arrumados os botões de continuar e iniciado o javascript para manipular o valor e tempo estimado na página do orçamento final

--- js/script.js
+++ js/script.js
@@ -58,16 +58,22 @@ function continuar(){
         window.location.href = "orcamento_celular.html"
     } else {
         if(tipo_orcamento == 2){
-            window.location.href = "orcamento_comp.html"
+            window.location.href = "orcamento_comp.html?type=" + tipo_orcamento;
         } else {
             if(tipo_orcamento == 3){
-                window.location.href = "contato.html"
+                window.location.href = "outro.html"
             }
         }
     }
 }
 
+
+//celular
+
 let marca_orcamento = 0;
+
+let tipo_reparo = 0;
+
 
 function marca(buttonId, valor) {
     document.querySelectorAll('.option2_cell').forEach(button => {
@@ -79,9 +85,7 @@ function marca(buttonId, valor) {
     console.log('marca = ' + marca_orcamento);
 }
 
-let tipo_reparo = 0;
-
-function reparo(buttonId, valor) {
+function reparo_celular(buttonId, valor) {
     document.querySelectorAll('.option2_reparo').forEach(button => {
         button.classList.remove('clicked');
     });
@@ -91,6 +95,93 @@ function reparo(buttonId, valor) {
     console.log('tipo de reparo = ' + tipo_reparo);
 }
 
-function continuar_final(){
-    window.location.href="orcamento_final.html"    
+function continuar_celular(){
+    if(marca_orcamento == 4 || tipo_reparo == 8){
+        window.location.href="outro.html"
+    } else {
+        window.location.href="orcamento_final.html?type=" + tipo_orcamento;    
+    }
+}
+
+
+//computador
+
+let tipo_reparo_comp = 0;
+
+function reparo_comp(buttonId, valor) {
+    document.querySelectorAll('.option2_reparo').forEach(button => {
+        button.classList.remove('clicked');
+    });
+    document.getElementById(buttonId).classList.add('clicked');
+
+    tipo_reparo_comp = valor;
+    console.log('tipo de reparo = ' + tipo_reparo_comp);
+}
+
+function continuar_comp(){
+    const urlParams = new URLSearchParams(window.location.search);
+    const tipo_orcamento_final = urlParams.get('type');
+
+    if(tipo_reparo_comp == 8){
+        window.location.href="outro.html"
+    } else {
+        window.location.href = "orcamento_final.html?type=" + tipo_orcamento_final + "&tipo_reparo=" + tipo_reparo_comp;    
+    }
+}
+
+function voltar_tipo(){
+    if(tipo_orcamento == 1){
+        window.location.href = "orcamento_celular.html"
+    } else {
+        window.location.href = "orcamento_comp.html"
+    }
+}
+
+//orçamento final
+
+let tipo_orcamento_final = 0;
+let tipo_reparo_comp_final = 0;
+
+function teste() {
+    const urlParams = new URLSearchParams(window.location.search);
+    tipo_orcamento_final = urlParams.get('type');
+    tipo_reparo_comp_final = urlParams.get('tipo_reparo');
+
+    console.log(tipo_orcamento_final);
+    console.log(tipo_reparo_comp_final);
+}
+
+function exibir_tempo(){
+
+    
+    let output_tempo = 0;
+    if(tipo_orcamento_final == 2){
+        switch (tipo_reparo_comp_final) {
+            case 1:
+                output_tempo = "1";
+                break;
+            case 2:
+                output_tempo = "2";
+                break;
+            case 3:
+                output_tempo = "3";
+                break;
+            case 4:
+                output_tempo = "4";
+                break;
+            case 5:
+                output_tempo = "5";
+                break;
+            case 6:
+                output_tempo = "6";
+                break;
+            case 7:
+                output_tempo = "7";
+                break;
+            case 8:
+                output_tempo = "8";
+                break;
+        }
+        console.log(output_tempo)
+    }
 }
